89c4b580c7dcfa0095f3cf11ef6b933f512b7499
SUMIT <sumeetborkar123@gmail.com>
2025-07-31 08:29:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.dirname(__file__))
-
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
