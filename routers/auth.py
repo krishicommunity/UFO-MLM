08ac4c45c2d3718f3f44cb7871109af033d8edaa
SUMIT <sumeetborkar123@gmail.com>
2025-07-31 08:34:25 +0530

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,3 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from database import db
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel, EmailStr
