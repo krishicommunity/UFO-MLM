2c5fdee29d61ad4664587ec386a3d6533cc0fb74
SUMIT <sumeetborkar123@gmail.com>
2025-08-01 15:56:39 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,9 +1,15 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URI = os.getenv("MONGO_URI")  # Make sure this is set correctly in Render
+MONGO_URI = os.getenv("MONGO_URI")
+
+print("✅ database.py loaded")
+print("✅ MONGO_URI:", MONGO_URI)
+
+if MONGO_URI is None:
+    raise Exception("❌ MONGO_URI environment variable is NOT SET!")
+
 client = MongoClient(MONGO_URI)
 db = client["ufo"]
 
-print("✅ MONGO_URI =", MONGO_URI)
-print("✅ DB initialized:", db)
+print("✅ DB Connection initialized:", db)
