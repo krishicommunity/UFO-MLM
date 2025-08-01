4f5234f95cbe7362e60ade02496d101d05349972
SUMIT <sumeetborkar123@gmail.com>
2025-08-01 15:26:23 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -4,3 +4,6 @@ import os
 MONGO_URI = os.getenv("MONGO_URI")  # Make sure this is set correctly in Render
 client = MongoClient(MONGO_URI)
 db = client["ufo"]
+
+print("✅ MONGO_URI =", MONGO_URI)
+print("✅ DB initialized:", db)
